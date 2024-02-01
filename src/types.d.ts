9da444dc387ebb7252e6a4cdd8117f77811527e6
Connor Mills <connor@virtualized.dev>
2024-02-01 21:33:58 +0000

    Cloned from MericcaN41

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,5 @@
-import { SlashCommandBuilder, CommandInteraction, Collection, PermissionResolvable, Message, AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js"
-import mongoose from "mongoose"
+import { SlashCommandBuilder, Collection, AutocompleteInteraction, ChatInputCommandInteraction, ModalSubmitInteraction, CacheType, Events } from 'discord.js'
+import mongoose from 'mongoose'
 
 export interface SlashCommand {
     command: SlashCommandBuilder,
@@ -7,14 +7,6 @@ export interface SlashCommand {
     autocomplete?: (interaction: AutocompleteInteraction) => void,
     modal?: (interaction: ModalSubmitInteraction<CacheType>) => void,
     cooldown?: number // in seconds
-}
-
-export interface Command {
-    name: string,
-    execute: (message: Message, args: Array<string>) => void,
-    permissions: Array<PermissionResolvable>,
-    aliases: Array<string>,
-    cooldown?: number,
 }
 
 interface GuildOptions {
@@ -31,25 +23,23 @@ export type GuildOption = keyof GuildOptions
 export interface BotEvent {
     name: string,
     once?: boolean | false,
-    execute: (...args?) => void
+    execute: (client, ...args?) => void
 }
 
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            TOKEN: string,
-            CLIENT_ID: string,
-            PREFIX: string,
-            MONGO_URI: string,
-            MONGO_DATABASE_NAME: string
+            DISCORD_TOKEN: string,
+            BOT_ID: string,
+            OWNER_ID: string,
+            GUILD_ID: string
         }
     }
 }
 
-declare module "discord.js" {
+declare module 'discord.js' {
     export interface Client {
-        slashCommands: Collection<string, SlashCommand>
-        commands: Collection<string, Command>,
+        commands: Collection<string, SlashCommand>
         cooldowns: Collection<string, number>
     }
 }
